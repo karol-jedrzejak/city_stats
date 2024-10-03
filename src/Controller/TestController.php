48aedd2cac6a84bdfac27d4707c671b49bfb9bfe
karol-jedrzejak <karol.jedrzejak@gmail.com>
2024-10-03 23:01:40 +0200

    up
    up

---
+++ src/Controller/TestController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', methods:['GET'], name: 'app_test.index')]
+    public function index(): Response
+    {
+        return new Response (
+            "<div>test</div>".
+            $this->generateUrl('app_test.index')
+        );
+    } 
+
+    #[Route('/test/{id}', methods:['GET'], name: 'app_test.nr')]
+    public function jakis_id(int $id): Response
+    {
+        return new Response (
+            "<div>test $id</div>".
+            $this->generateUrl('app_test.index')
+        );
+    } 
+
+/*     #[Route('/test', name: 'app_test')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TestController.php',
+        ]);
+    } */
+}
